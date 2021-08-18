184c8dd2f4bde7bd69c46d98a8edde2dc333716e
waso47 <jckwason@gmail.com>
2021-08-18 10:04:24 +1000

    Delete HelloWorldApp.java

--- HelloWorldApp.java
+++
@@ -1,8 +0,0 @@
-class HelloWorldApp{
-    public static void main(String[] args){
-        //Display "Hello world!"
-        //System.out.println("Hello world!");
-        System.out.print(args[0]);
-        System.out.println(", " + "world!");
-    }
-}
