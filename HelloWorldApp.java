68b5910aec6c99edc282384afb49db0a3160531e
waso47 <jckwason@gmail.com>
2021-08-18 10:03:52 +1000

    Add files via upload

---
+++ HelloWorldApp.java
@@ -0,0 +1,8 @@
+class HelloWorldApp{
+    public static void main(String[] args){
+        //Display "Hello world!"
+        //System.out.println("Hello world!");
+        System.out.print(args[0]);
+        System.out.println(", " + "world!");
+    }
+}
